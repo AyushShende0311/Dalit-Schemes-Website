b2467af5eb723b41158c81717c3b45b06e12de32
Ayush Shende <ayushshende10@gmail.com>
2022-03-24 10:59:35 +0530

    scripts added

---
+++ FullStackProject/scripts/district.js
@@ -0,0 +1,18 @@
+
+
+var selectDistrict = document.getElementById("selDistrict");
+
+
+// selectDistrict.onchange(function(){
+//     var selected = selectDistrict. options[selectDistrict.selectedIndex].text;
+//     alert(selected);
+// });
+
+var list = ["a" , "b", "c"];
+selectDistrict.appendChild
+
+selectDistrict.addEventListener("change", ()=>{
+    var selected = selectDistrict.selectedIndex;
+    document.getElementById("distAreas").style.display = "block";
+
+});
